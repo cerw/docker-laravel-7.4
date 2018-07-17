9c29967f94971bf65aef0cf28d137c4776d630fe
Petr Cervenka <petr@nanosolutions.io>
2018-07-17 12:56:32 +0800

    Set time

--- provision/provision.sh
+++ provision/provision.sh
@@ -12,7 +12,10 @@ apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold"
 # install PHP
 apt-get -y install curl php-zip zip php7.2-gd unzip bzip2 php-cli php-imagick imagemagick git \
 php-curl php-xml  php7.2-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-xdebug \
-memcached php-memcached php7.2-soap build-essential libpng-dev
+memcached php-memcached php7.2-soap build-essential libpng-dev openntpd
+
+
+/usr/bin/timedatectl set-timezone Australia/Sydney
 
 #phpdismod xdebug
 #hpdismod -s cli xdebug
