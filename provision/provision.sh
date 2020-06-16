bf32bbd005a836ee23cba4df9ab5cd2303012290
Petr Cervenka <petr@nanosolutions.io>
2020-06-16 16:11:52 +0800

    Add xvfb

--- provision/provision.sh
+++ provision/provision.sh
@@ -15,7 +15,7 @@ php-curl php-xml  php7.2-sqlite3 php-mbstring php-xml php-mysqlnd php-curl php-x
 memcached php-memcached php7.2-soap build-essential libpng-dev openntpd php7.2-bcmath mysql-client-5.7 ghostscript
 
 # install latex
-apt-get -y install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra  texlive-latex-extra latexmk
+apt-get -y install xvfb texlive-latex-base texlive-fonts-recommended texlive-fonts-extra  texlive-latex-extra latexmk
 
 /usr/bin/timedatectl set-timezone Australia/Sydney
 
